ac1c86cff53e19c723090587db6f3e8c5542e034
Michael Byers <mikebyers24@gmail.com>
2024-06-25 12:02:23 -0400

    register form.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,164 @@
-const Home = () => {
-  return <div>Tryp Transit</div>;
+'use client';
+
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import bcrypt from 'bcryptjs';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+
+const registerFormSchema = z
+  .object({
+    name: z.string().min(1, 'Must be a valid name'),
+    email: z.string().email('Must be a valid email.'),
+    password: z.string().min(1, 'Must be a valid password'),
+    confirmPassword: z.string().min(1, 'Must be a valid password'),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: 'Passwords must match',
+    path: ['confirmPassword'],
+  });
+
+const RegisterForm = () => {
+  const router = useRouter();
+  const form = useForm({
+    resolver: zodResolver(registerFormSchema),
+    defaultValues: {
+      name: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
+    },
+  });
+
+  async function onSubmit(data: z.infer<typeof registerFormSchema>) {
+    const users = JSON.parse(localStorage.getItem('users') || '[]');
+    const hashedPassword = await bcrypt.hash(data.password, 12);
+    users.push({
+      name: data.name,
+      email: data.email,
+      password: hashedPassword,
+    });
+
+    localStorage.setItem('users', JSON.stringify(users));
+    router.push('/login');
+  }
+
+  return (
+    <div className="flex justify-center mt-8">
+      <Card className="max-w-md w-11/12 shadow-xl">
+        <CardHeader>
+          <CardTitle className="text-center text-primary font-bold">
+            Register
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form
+              className="flex flex-col gap-4"
+              onSubmit={form.handleSubmit(onSubmit)}
+            >
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Name:</FormLabel>
+                    <FormControl>
+                      <Input
+                        className="w-full"
+                        type="text"
+                        placeholder="Name"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email:</FormLabel>
+                    <FormControl>
+                      <Input
+                        className="w-full"
+                        type="text"
+                        placeholder="Email"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password:</FormLabel>
+                    <FormControl>
+                      <Input
+                        className="w-full"
+                        type="password"
+                        placeholder="******"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="confirmPassword"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Confirm Password:</FormLabel>
+                    <FormControl>
+                      <Input
+                        className="w-full"
+                        type="password"
+                        placeholder="******"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button variant="secondary" type="submit">
+                Register
+              </Button>
+            </form>
+          </Form>
+          <div className="mt-2 text-center">
+            Already have an account?{' '}
+            <Link
+              className="font-bold hover:underline text-primary"
+              href="/login"
+            >
+              Login here
+            </Link>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  );
 };
 
-export default Home;
+export default RegisterForm;
